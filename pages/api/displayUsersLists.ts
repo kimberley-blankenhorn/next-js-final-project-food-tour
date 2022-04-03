dc44c144c263462e107b0ace3a0fdddec4f9f996
Kimberley Blankenhorn <kimberley.blankenhorn@gmail.com>
2022-04-03 22:56:20 +0200

    Fix eslint problems

--- pages/api/displayUsersLists.ts
+++ pages/api/displayUsersLists.ts
@@ -1,11 +1,15 @@
+/* eslint-disable import/no-unresolved */
 import { NextApiRequest, NextApiResponse } from 'next';
+// eslint-disable-next-line import/no-unresolved
+import { getAllUsersLists } from '../../util/database';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   console.log('request is', req.query);
-  const displayAllLists = await getAllUsersLists(req.query);
+  const displayAllLists = await getAllUsersLists;
+  // (req.query);
   console.log('returned data', displayAllLists);
   return res.status(200).json({ listAll: displayAllLists });
 }
